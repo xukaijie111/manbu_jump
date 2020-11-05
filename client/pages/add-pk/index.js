f769ddf6b8d8b4a9a2871d111acb696e7a55c38e
xukaijie <xukaijie2009@163.com>
2020-11-05 14:54:33 +0800

    commit

--- client/pages/add-pk/index.js
+++ client/pages/add-pk/index.js
@@ -1,22 +1,71 @@
 // pages/add-pk/index.js
+import Storage from '../../utils/storage'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    hour:'',
+    minute:'',
     mode:1, // 1时间 2 //计数
     items: [
       {value: 1, name: '计时跳',checked:true},
       {value:2, name: '计数跳',checked:false},
+    ],
+    countArray:[
+      50,
+      100,
+      300,
+      500,
+      1000,
+      1500,
+      2000,
+      3000,
+      4000,
+      5000,
     ]
+  },
+
+  radioChange(e) {
+    const value = e.detail.value;
+    this.setData({
+      mode:parseInt(value)
+    })
+  },
+
+  bindPickerChange(e) {
+    const index = parseInt(e.detail.value);
+    var count = this.data.countArray[index];
+    this.setData({
+      count
+    })
+    Storage.count = count
+  },
+  
+  bindTimeChange: function(e) {
+    let time = e.detail.value;
+    const hour = time.split(':')[0]
+    const minute = time.split(':')[1]
+    this.setData({
+      time,
+      hour,
+      minute,
+    })
+    Storage.hour = hour;
+    Storage.minute = minute;
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var count = Storage.count || 100
+    var hour = Storage.hour || 0
+    var minute = Storage.minute || 30
+    this.setData({
+      minute,count,hour
+    })
   },
 
   /**
